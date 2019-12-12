f09c59d844b619e6e8f3196b6ec2ced464aeaa8c
杜郡 <95609658@qq.com>
2019-12-12 16:12:37 +0800

    build: update npm publish deploy config

--- packages/chat/src/main.vue
+++ packages/chat/src/main.vue
@@ -16,16 +16,3 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-.chat-wrapper {
-  width: 800px;
-  height: 550px;
-  background: #eee;
-  .chat-box {
-    position: relative;
-    display: flex;
-    widows: 100%;
-    height: 100%;
-  }
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,11 @@
 const path = require('path');
 module.exports = {
-  outputDir: 'lib',
   productionSourceMap: false,
-  configureWebpack: {
-    entry: {
-      app: ['./src/index.js']
-    },
-    output: {
-      filename: 'ideal-ui.common.js',
-      chunkFilename: '[id].js',
-      libraryExport: 'default',
-      library: 'IDEAL',
-      libraryTarget: 'commonjs2'
+  pages: {
+    index: {
+      entry: './examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
     }
   }
 };
