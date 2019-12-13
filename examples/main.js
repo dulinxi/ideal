ba335be4b11f40474792416f49dfd86737b18ed2
杜郡 <95609658@qq.com>
2019-12-13 09:46:03 +0800

    doc: add init css

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import IdealUI from '../src/index.js';
 import '../lib/theme-ideal/index.css';
+import './assets/scss/index.scss';
 
 import App from './App';
 Vue.use(IdealUI);
+console.log(1);
 new Vue({
   el: '#app',
   render: h => h(App)
